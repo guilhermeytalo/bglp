ac1d18d8a5c267261c78ee6c2b1e9222f0840cb6
Guilherme Ytalo <gydaytalo@gmail.com>
2022-04-26 00:17:39 -0300

    project folder structure

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/header/header';
+
+const Home = () =>  {
+  return (
+    <div>
+        <Header />
+      <p>Home Work's</p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/variables/colors.ts
@@ -0,0 +1,9 @@
+const Colors = {
+    primary: '#1BCF92',
+    black: '#2D2D2D',
+    white: '#FFFFFF',
+    grey: '#F0F0F0',
+    yellow: '#FFC839'
+}
+
+export default Colors;
